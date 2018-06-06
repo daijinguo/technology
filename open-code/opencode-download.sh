0189ad34571deabbf6a4554684470de23294cf34
YourName <YourEmail@host.com>
2018-06-06 22:36:47 +0800

    update the opencode file

--- open-code/opencode-download.sh
+++ open-code/opencode-download.sh
@@ -13,27 +13,21 @@ mkdir -p android/player
 cd android/player
 
 # I.1 NiceVieoPlayer
-if [ ! -d "01-nvp" ]; then
+if [ ! -d "01.NiceVieoPlayer" ]; then
     echo "download the NiceVieoPlayer ..."
-    git clone https://github.com/xiaoyanger0825/NiceVieoPlayer.git 01-nvp
+    git clone https://github.com/xiaoyanger0825/NiceVieoPlayer.git 01.NiceVieoPlayer
 fi
 
-# I.2  HMCPlayer
-if [ ! -d "02-hmcplayer" ]; then
-    echo "download the HMCPlayer ..."
-    git clone https://github.com/emacs2012/HMCPlayer.git 02-hmcplayer
-fi
-
-# I.3  ijkplayer
-if [ ! -d "ijkplayer" ]; then
+# I.2  ijkplayer
+if [ ! -d "02.ijkplayer" ]; then
     echo "https://github.com/Bilibili/ijkplayer.git"
-    git clone https://github.com/Bilibili/ijkplayer.git ijkplayer
+    git clone https://github.com/Bilibili/ijkplayer.git 02.ijkplayer
 fi
 
-# I.4 ExoPlayer
-if [ ! -d "ExoPlayer" ]; then
+# I.3 ExoPlayer
+if [ ! -d "03.ExoPlayer" ]; then
     echo "https://github.com/google/ExoPlayer.git"
-    git clone https://github.com/google/ExoPlayer.git
+    git clone https://github.com/google/ExoPlayer.git 03.ExoPlayer
 fi
 
 
@@ -45,48 +39,48 @@ cd $DEF_DIR
 mkdir -p module
 cd module
 
-# II.1  HMCPlayer
+# II.1  360RePlugin
 if [ ! -d "01-360RePlugin" ]; then
    echo "download the 360RePlugin ..."
    git clone https://github.com/Qihoo360/RePlugin.git 01-360RePlugin
 fi
 
 # II.2  ModularizationArchitecture
-if [ ! -d "02-ma" ]; then
+if [ ! -d "02.ModularizationArchitecture" ]; then
    echo "download the ModularizationArchitecture ..."
-   git clone https://github.com/SpinyTech/ModularizationArchitecture.git 02-ma
+   git clone https://github.com/SpinyTech/ModularizationArchitecture.git 02.ModularizationArchitecture
 fi
 
 # II.3  DDComponentForAndroid
-if [ ! -d "03-ddc" ]; then
+if [ ! -d "03.DDComponentForAndroid" ]; then
    echo "download the DDComponentForAndroid ..."
-   git clone https://github.com/luojilab/DDComponentForAndroid.git 03-ddc
+   git clone https://github.com/luojilab/DDComponentForAndroid.git 03.DDComponentForAndroid
 fi
 
 # II.4  fat-aar-plugin
-if [ ! -d "04-fap" ]; then
+if [ ! -d "04.fat-aar-plugin" ]; then
    echo "download the fat-aar-plugin ..."
-   git clone https://github.com/Vigi0303/fat-aar-plugin.git 04-fap
+   git clone https://github.com/Vigi0303/fat-aar-plugin.git 04.fat-aar-plugin
 fi
 
 # II.5  AndroidModulePattern
-if [ ! -d "05-ModulePattern" ]; then
+if [ ! -d "05.AndroidModulePattern" ]; then
    echo "download the fat-aar-plugin ..."
-   git clone https://github.com/guiying712/AndroidModulePattern.git 05-ModulePattern
+   git clone https://github.com/guiying712/AndroidModulePattern.git 05.AndroidModulePattern
 fi
 
 # II.6  ComponentCaller
 echo "https://github.com/luckybilly/CC"
-if [ ! -d "06-ComponentCaller" ]; then
+if [ ! -d "06.ComponentCaller" ]; then
    echo "download the fat-aar-plugin ..."
-   git clone https://github.com/guiying712/AndroidModulePattern.git 06-ComponentCaller
+   git clone https://github.com/guiying712/AndroidModulePattern.git 06.ComponentCaller
 fi
 
 # II.7  VirtualAPK
 echo "https://github.com/didi/VirtualAPK.git"
-if [ ! -d "VirtualAPK" ]; then
+if [ ! -d "07.VirtualAPK" ]; then
    echo "download the fat-aar-plugin ..."
-   git clone https://github.com/didi/VirtualAPK.git VirtualAPK
+   git clone https://github.com/didi/VirtualAPK.git 07.VirtualAPK
 fi
 
 
@@ -101,39 +95,39 @@ mkdir -p $DEF_GOOGLESAMPLE_DIR
 cd $DEF_GOOGLESAMPLE_DIR
 
 # III.1  androidtv-Leanback
-if [ ! -d "01-tv.leanback" ]; then
+if [ ! -d "01.androidtv-Leanback" ]; then
    echo "download the androidtv-Leanback ..."
-   git clone https://github.com/googlesamples/androidtv-Leanback.git 01-tv.leanback
+   git clone https://github.com/googlesamples/androidtv-Leanback.git 01.androidtv-Leanback
 fi
 
 # III.2  AndroidTVappTutorial
-if [ ! -d "02-tv.tutorial" ]; then
+if [ ! -d "02.AndroidTVappTutorial" ]; then
    echo "download the AndroidTVappTutorial ..."
-   git clone https://github.com/corochann/AndroidTVappTutorial.git 02-tv.tutorial
+   git clone https://github.com/corochann/AndroidTVappTutorial.git 02.AndroidTVappTutorial
 fi
 
 # III.3  android-architecture
-if [ ! -d "03-architecture" ]; then
+if [ ! -d "03.android-architecture" ]; then
    echo "download the android-architecture ..."
-   git clone https://github.com/googlesamples/android-architecture.git 03-architecture
+   git clone https://github.com/googlesamples/android-architecture.git 03.android-architecture
 fi
 
 # III.4  android-BasicMediaDecoder
-if [ ! -d "04-basicMediaDecoder" ]; then
+if [ ! -d "04.android-BasicMediaDecoder" ]; then
    echo "download the android-BasicMediaDecoder ..."
-   git clone https://github.com/googlesamples/android-BasicMediaDecoder.git 04-basicMediaDecoder
+   git clone https://github.com/googlesamples/android-BasicMediaDecoder.git 04.android-BasicMediaDecoder
 fi
 
 # III.5  android-MediaBrowserService
-if [ ! -d "05-MediaBrowserService" ]; then
+if [ ! -d "05.android-MediaBrowserService" ]; then
    echo "download the android-MediaBrowserService ..."
-   git clone https://github.com/googlesamples/android-MediaBrowserService.git 05-MediaBrowserService
+   git clone https://github.com/googlesamples/android-MediaBrowserService.git 05.android-MediaBrowserService
 fi
 
 # III.6  leanback-showcase
-if [ ! -d "06-ls" ]; then
+if [ ! -d "06.leanback-showcase" ]; then
    echo "download the leanback-showcase ..."
-   git clone https://github.com/googlesamples/leanback-showcase.git 06-ls
+   git clone https://github.com/googlesamples/leanback-showcase.git 06.leanback-showcase
 fi
 
 # part III} +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -146,15 +140,15 @@ mkdir -p other
 cd other
 
 # IV.1  AndroidNativeService-meets-ASHMEM
-if [ ! -d "01-ashmem" ]; then
+if [ ! -d "01.ashmem" ]; then
    echo "download the AndroidNativeService-meets-ASHMEM ..."
-   git clone https://github.com/alximw/AndroidNativeService-meets-ASHMEM.git 01-ashmem
+   git clone https://github.com/alximw/AndroidNativeService-meets-ASHMEM.git 01.ashmem
 fi
 
 # IV.2  kotlin-life
-if [ ! -d "02-kf" ]; then
+if [ ! -d "02.kotlin-life" ]; then
    echo "download the kotlin-life ..."
-   git clone https://github.com/Cuieney/kotlin-life.git 02-kf
+   git clone https://github.com/Cuieney/kotlin-life.git 02.kotlin-life
 fi
 
 
@@ -166,19 +160,26 @@ cd $DEF_DIR
 mkdir -p myselft
 cd myselft
 
-if [ ! -d "androidplayer" ]; then
+if [ ! -d "01.androidplayer" ]; then
    echo "download androidplayer  ..."
-   git clone https://github.com/goto0x00/androidplayer.git
+   git clone https://github.com/goto0x00/androidplayer.git 01.androidplayer
+   git config user.name "YourName"
+   git config user.email "YourEmail@host.com"
 fi
 
-if [ ! -d "affmpeg" ]; then
+if [ ! -d "02.affmpeg" ]; then
    echo "https://github.com/goto0x00/affmpeg.git"
-   git clone https://github.com/goto0x00/affmpeg.git
+   git clone https://github.com/goto0x00/affmpeg.git 02.affmpeg
 fi
 
-if [ ! -d "dplayer" ]; then
+#if [ ! -d "03.dplayer" ]; then
+#   echo "https://git.coding.net/gooogle/dplayer.git"
+#   git clone https://git.coding.net/gooogle/dplayer.git 03.dplayer
+#fi
+
+if [ ! -d "03.sdl-game" ]; then
    echo "https://git.coding.net/gooogle/dplayer.git"
-   git clone https://git.coding.net/gooogle/dplayer.git
+   git clone https://github.com/daijinguo/sdl-game.git 03.sdl-game
 fi
 
 # part V} +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
